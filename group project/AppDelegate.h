ea12c71bac5a1c8a109d2326d9ecc910bf00a494
rlthomps <rlthomps@rlthomps-macbookair2.roam.corp.google.com>
2014-07-13 16:33:34 -0700

    Initial Commit

---
+++ group project/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  group project
+//
+//  Created by Robert Thompson on 7/13/14.
+//  Copyright (c) 2014 Google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
